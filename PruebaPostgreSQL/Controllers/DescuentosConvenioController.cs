ed7efc5a21f32ed1fa060d623163ff8f11dc8646
juan diego tonguino <juand.wwe@hotmail.com>
2019-09-07 00:09:05 -0500

    primera version

---
+++ PruebaPostgreSQL/Controllers/DescuentosConvenioController.cs
@@ -0,0 +1,223 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using PruebaPostgreSQL.Models;
+
+namespace PruebaPostgreSQL.Controllers
+{
+    public class DescuentosConvenioController : Controller
+    {
+        private List<DescuentoPorConvenio> _arrayListDescuentos;
+
+        private List<DescuentoPorConvenio> _lstDescuentosIndividuales;
+
+        private string[] _lstSucursalesFilter;
+
+        private int _proximoNumeroDisponible;
+
+        private string[] _tipoDocuemnto;
+
+        private string _detalle;
+
+        private DateTime _fecha;
+
+        private string _tipoDocumento;
+
+        // GET: DescuentosConvenio
+        public ActionResult Index()
+        {
+            _arrayListDescuentos = new List<DescuentoPorConvenio>() {
+                new DescuentoPorConvenio(){
+                    Id=1,
+                    Sucursal="Pasto",
+                    Convenio="Convenio1",
+                    Identifnicacion="1085309822",
+                    Nombre="juan",
+                    Modulo="modulo1",
+                    Concepto="conepto1",
+                    Credito="credito1",
+                    FechaVencimiento= DateTime.Now,
+                    Cuota="130,000",
+                    ValorDefinitivo="130,000",
+                    FormaPago="Efecivo",
+                    ValorDistinto=""
+                },
+                new DescuentoPorConvenio(){
+                    Id=2,
+                    Sucursal="Cali",
+                    Convenio="Convenio2",
+                    Identifnicacion="1085309824",
+                    Nombre="diego",
+                    Modulo="modulo2",
+                    Concepto="conepto2",
+                    Credito="credito2",
+                    FechaVencimiento= DateTime.Now,
+                    Cuota="149,000",
+                    ValorDefinitivo="149,000",
+                    FormaPago="tarjeta",
+                    ValorDistinto=""
+                },
+                new DescuentoPorConvenio(){
+                    Id=3,
+                    Sucursal="Bogota",
+                    Convenio="Convenio3",
+                    Identifnicacion="1085309825",
+                    Nombre="Hernan",
+                    Modulo="modulo3",
+                    Concepto="conepto3",
+                    Credito="credito3",
+                    FechaVencimiento= DateTime.Now,
+                    Cuota="150,000",
+                    ValorDefinitivo="150,000",
+                    FormaPago="efectivo",
+                    ValorDistinto=""
+                }
+            };
+            _lstDescuentosIndividuales = new List<DescuentoPorConvenio>() {
+                new DescuentoPorConvenio(){
+                    Id=4,
+                    Sucursal="Pasto",
+                    Convenio="Convenio1",
+                    Identifnicacion="1085309822",
+                    Nombre="juan",
+                    Modulo="modulo1",
+                    Concepto="conepto1",
+                    Credito="credito1",
+                    FechaVencimiento= DateTime.Now,
+                    Cuota="130,000",
+                    ValorDefinitivo="130,000",
+                    FormaPago="Efecivo",
+                    ValorDistinto=""
+                },
+                new DescuentoPorConvenio(){
+                    Id=5,
+                    Sucursal="Cali",
+                    Convenio="Convenio2",
+                    Identifnicacion="1085309824",
+                    Nombre="diego",
+                    Modulo="modulo2",
+                    Concepto="conepto2",
+                    Credito="credito2",
+                    FechaVencimiento= DateTime.Now,
+                    Cuota="149,000",
+                    ValorDefinitivo="149,000",
+                    FormaPago="tarjeta",
+                    ValorDistinto=""
+                },
+                new DescuentoPorConvenio(){
+                    Id=6,
+                    Sucursal="Bogota",
+                    Convenio="Convenio3",
+                    Identifnicacion="1085309825",
+                    Nombre="Hernan",
+                    Modulo="modulo3",
+                    Concepto="conepto3",
+                    Credito="credito3",
+                    FechaVencimiento= DateTime.Now,
+                    Cuota="150,000",
+                    ValorDefinitivo="150,000",
+                    FormaPago="efectivo",
+                    ValorDistinto=""
+                }
+            };
+            _proximoNumeroDisponible = 123432;
+            _lstSucursalesFilter= new string[] { "Pasto", "Cali", "Bogota" };
+            _tipoDocuemnto = new string[] { "Descuentos Por Documento" };
+            _fecha = DateTime.Now;
+            _detalle = "";
+
+            dynamic render = new ExpandoObject();
+            render.lstDecuentos = JsonConvert.SerializeObject(_arrayListDescuentos);
+            render.lstDescuentosIndividuales = JsonConvert.SerializeObject(_lstDescuentosIndividuales);
+            render.proximoNumeroDisponible = _proximoNumeroDisponible;
+            render.sucursalFilter = _lstSucursalesFilter;
+            render.tipoDocumento = _tipoDocuemnto;
+            render.descripcion = _detalle;
+            render.fecha = _fecha;
+            return View(render);
+        }
+
+        // GET: DescuentosConvenio/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: DescuentosConvenio/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: DescuentosConvenio/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                _tipoDocumento=collection["TipoDocumento"];
+                _detalle = collection["detalle"];
+                _fecha=DateTime.ParseExact(collection["Fecha"], "yyyy-MM-dd", CultureInfo.InvariantCulture);
+                _proximoNumeroDisponible = int.Parse(collection["ProximoNumeroDisponible"]);
+                string tempDescuentos = collection["descuentosSaved"];
+                List<DescuentoPorConvenio> resutlt = JsonConvert.DeserializeObject<List<DescuentoPorConvenio>>(tempDescuentos);
+                _arrayListDescuentos = resutlt;
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DescuentosConvenio/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: DescuentosConvenio/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: DescuentosConvenio/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: DescuentosConvenio/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ PruebaPostgreSQL/Scripts/DescuentosConvenios.js
@@ -0,0 +1,232 @@
+﻿var descuentosArray = getArray("jsonObjct");
+var descuentosIndividualArray = getArray("jsonObjctIndividual");
+var seleccionConsolidaddo = [];
+var seleccionIndividual = [];
+
+$('.select-custom-multiple').select2();
+$('.dx-toolbar-after´').addClass("hideElement");
+
+function getArray(id) {
+    temp = JSON.parse($("#"+id).val());
+    for (i = 0; i < temp.length; i++) {
+        temp[i].FechaVencimiento = new Date(temp[i].FechaVencimiento);
+    }
+    return temp;
+}
+
+function getDataGridInstance() {
+    return $("#gridContainerConsolidado").dxDataGrid("instance");
+}
+
+function getDataDetalleInstance() {
+    return $("#gridContainerDestalle").dxDataGrid("instance");
+}
+
+function getDataIndividualInstance() {
+    return $("#gridContainerIndividual").dxDataGrid("instance");
+}
+
+function selection_changed_consolidado(selectedItems) {
+    seleccionConsolidaddo = selectedItems.selectedRowsData;
+    console.log(seleccionConsolidaddo);
+}
+
+function selection_changed_individual(selectedItems) {
+    seleccionIndividual = selectedItems.selectedRowsData;
+    console.log(seleccionIndividual);
+}
+
+function actionBtnConsultarGeneral(e) {
+    let vencidosHasta = $('#vencimientosHasta').val();
+    vencidosHasta=vencidosHasta.replace(/-/g, '\/');
+    let sucursal = $('#sucursal').val();
+    let convenio = $('#convenio').val();
+    let modulo = $('#modulo').val();
+    let formaPago = $('#formaPago').val();
+    let filterTable = buildFilter(vencidosHasta, sucursal, convenio, modulo, formaPago);
+    //tabla consolidado
+    getDataGridInstance().clearFilter();
+    getDataGridInstance().filter(filterTable);
+    //tabla detalle
+    getDataDetalleInstance().clearFilter();
+    getDataDetalleInstance().filter(filterTable);
+}
+
+function actiondConsultarIndivudual(e) {
+    let vencidosHastaI = $('#vencimientosHastaI').val();
+    vencidosHastaI = vencidosHastaI.replace(/-/g, '\/');
+    let sucursalI = $('#sucursalI').val();
+    let convenioI = $('#convenioI').val();
+    let identificaionI = $("#identificaionI").val();
+    let moduloI = $('#moduloI').val();
+    let formaPagoI = $('#formaPagoI').val();
+    if (!(vencidosHastaI == "" && sucursalI == null && convenioI=="" && identificaionI==""&& moduloI==""&& formaPagoI == "")) {
+        $("#individualDatatable").removeClass("hideElement");
+        $('#botonagregarindividual').removeClass("hideElement");
+    }
+    let filterTableI = buildFilterIndividual(vencidosHastaI, sucursalI, convenioI, moduloI, formaPagoI, identificaionI);
+    //tabla consolidado
+    //getDataIndividualInstance().clearFilter();
+    getDataIndividualInstance().clearFilter();
+    getDataIndividualInstance().filter(filterTableI);
+}
+
+function preparateArrayFilter(columnName, array) {
+    let retorno = [];
+    if (array === null) {
+        return retorno;
+    }
+
+    for (i = 0; i < array.length; i++) {
+        arrayValue = [columnName, "contains", array[i]];
+        retorno.push(arrayValue);
+        if (!(i + 1 === array.length)) {
+            retorno.push("or");
+        }
+    }
+    return retorno;
+}
+
+function buildFilter(vencidosHasta, sucursal, convenio, modulo, formaPago) {
+    let retorno = [
+        ["Convenio", "contains", convenio],
+        "and",
+        ["Modulo", "contains", modulo],
+        "and",
+        ["FormaPago", "contains", formaPago]
+    ];
+    arraySucursalFilter = preparateArrayFilter("Sucursal", sucursal);
+
+    if (arraySucursalFilter.length !== 0) {
+        retorno.push("and");
+        retorno.push(arraySucursalFilter);
+    }
+    if (vencidosHasta !== "") {
+        retorno.push("and");
+        retorno.push(["FechaVencimiento", "<=", new Date(vencidosHasta).addHours(24)]);
+    }
+    return retorno;
+}
+
+function buildFilterIndividual(pvencidosHastaI, psucursalI, pconvenioI, pmoduloI, pformaPagoI, pidentificaionI) {
+    let retorno = [
+        ["Convenio", "contains", pconvenioI],
+        "and",
+        ["Modulo", "contains", pmoduloI],
+        "and",
+        ["FormaPago", "contains", pformaPagoI],
+        "and",
+        ["Identifnicacion", "contains", pidentificaionI]
+    ];
+    arraySucursalFilter = preparateArrayFilter("Sucursal", psucursalI);
+
+    if (arraySucursalFilter.length !== 0) {
+        retorno.push("and");
+        retorno.push(arraySucursalFilter);
+    }
+    if (pvencidosHastaI !== "") {
+        retorno.push("and");
+        retorno.push(["FechaVencimiento", "<=", new Date(pvencidosHastaI).addHours(24)]);
+    }
+    return retorno;
+}
+
+function limpiarConsultaIndividual(e) {
+    $('#vencimientosHastaI').val("");
+    $('#sucursalI').val("");
+    $('#convenioI').val("");
+    $("#identificaionI").val("");
+    $('#moduloI').val("");
+    $('#formaPagoI').val("");
+    $('.select-custom-multiple').val(null).trigger("change");
+    $("#individualDatatable").addClass("hideElement");
+    $('#botonagregarindividual').addClass("hideElement");
+    getDataIndividualInstance().clearFilter();
+}
+
+function limpiarConsultaConsolidado(e) {
+    $('#vencimientosHasta').val("");
+    $('#sucursal').val("");
+    $('#convenio').val("");
+    $('#modulo').val("");
+    $('#formaPago').val("");
+    $('.select-custom-multiple').val(null).trigger("change");
+    getDataGridInstance().clearFilter();
+    getDataDetalleInstance().clearFilter();
+}
+
+function elminarRegistroConsolidado(e) {
+    for (i = 0; i < seleccionConsolidaddo.length; i++) {        
+        index = descuentosArray.indexOf(seleccionConsolidaddo[i]);
+        descuentosArray.splice(index, 1);
+    }
+    getDataGridInstance().refresh();
+    getDataDetalleInstance().refresh();
+    getDataIndividualInstance().refresh();
+}
+
+function guardar(e) {
+    limpiarConsultaConsolidado(null);
+    getDataGridInstance().saveEditData(); 
+    updateArrayConsolidado();
+    getDataDetalleInstance().refresh();
+    $('#detalleTab').click();
+}
+
+function updateArrayConsolidado() {
+    for (i = 0; i < descuentosArray.length; i++) {
+        tempValue = descuentosArray[i].ValorDistinto;
+        tempDefStr = descuentosArray[i].ValorDefinitivo;
+        if (tempValue !== "") {
+            tempFloatDistinct = parseFloat(tempValue.replace(/,/g, ""));
+            temoDef = parseFloat(tempDefStr.replace(/,/g, ""));
+            descuentosArray[i].ValorDefinitivo = formatNumbrer(tempFloatDistinct);
+        }
+    }
+}
+
+function adicionar(e) {
+    getDataIndividualInstance().saveEditData();
+    for (i = 0; i < seleccionIndividual.length; i++) {
+        descuentosArray.push(seleccionIndividual[i]);
+        index = descuentosIndividualArray.indexOf(seleccionIndividual[i]);
+        descuentosIndividualArray.splice(index, 1);
+    }
+    seleccionIndividual = [];
+    getDataIndividualInstance().refresh();
+    getDataGridInstance().refresh();
+    getDataDetalleInstance().refresh();
+    $('#generalTab').click();
+}
+
+function validateForm() {
+    let retorno = true;
+    fecha = $("#fecha").val();
+    detalle = $("#detalle").val();
+    if (detalle === "") {
+        retorno = false;
+        DevExpress.ui.notify("El campo detalle no puede ser nulo", "error", 6000);
+    }
+    if (fecha === "") {
+        retorno = false;
+        DevExpress.ui.notify("El campo fecha no puede ser nulo", "error", 6000);
+    }
+    return retorno;
+}
+
+function enviarAction(e) {
+    if (validateForm()) {
+        sendFrom("Create");
+    }
+}
+
+function formatNumbrer(number) {
+    return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function sendFrom(route) {
+    $("#jsonToSend").val(JSON.stringify(descuentosArray));
+    $("#proximoNumeroDisponible").removeAttr("disabled");
+    $("#FromData").attr("action", route);
+    $("#FromData").submit();
+}
